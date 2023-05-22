4eeab11d2fac71462d5e5ad4c47d0dce841a4127
prothom-acharjee <124777330+prothom-acharjee@users.noreply.github.com>
2023-05-22 17:59:31 +0600

    changes

--- resources/views/backend/stories/comments.blade.php
+++ resources/views/backend/stories/comments.blade.php
@@ -10,7 +10,7 @@
                 <ol class="breadcrumb mb-0 p-0">
                     <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}"><i class="bx bx-home-alt"></i></a>
                     </li>
-                    <li class="breadcrumb-item active" aria-current="page">Stories</li>
+                    <li class="breadcrumb-item active" aria-current="page">Story Comments</li>
                 </ol>
             </nav>
         </div>
